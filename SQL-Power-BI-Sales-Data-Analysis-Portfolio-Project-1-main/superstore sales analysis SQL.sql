79e72e6e55cc3f7b58a458321a4ac29fa1cefa6e
MohamedAsif08 <mohamed.asif.2k23@gmail.com>
2025-04-30 21:40:43 -0700

    new change

--- SQL-Power-BI-Sales-Data-Analysis-Portfolio-Project-1-main/superstore sales analysis SQL.sql
+++ SQL-Power-BI-Sales-Data-Analysis-Portfolio-Project-1-main/superstore sales analysis SQL.sql
@@ -10,25 +10,29 @@ SUM(sales) AS Total_sales,
 SUM(quantity) AS Total_quantity_sold,
 AVG(profit) AS Avg_profit,
 AVG(discount) AS Total_discount
-FROM dbo.SuperStoreOrders
+FROM Sales_Data
 
 -- sales performance analysis --
-SELECT TOP 10
+Select product_name, category, Total_sales, Total_quantity_sold from
+(
+Select
 product_name,
 category,
-SUM(sales) AS Toatl_sales,
+SUM(sales) AS Total_sales,
 SUM(quantity) AS Total_quantity_sold
-FROM dbo.SuperStoreOrders
+FROM sales_data
 GROUP BY
 product_name,
 category
 ORDER BY 
 SUM(sales) DESC
+)
+where Rownum <= 10
 --sales over year --
 SELECT 
 year,
 SUM(sales) AS Total_sales
-FROM dbo.SuperStoreOrders
+FROM sales_data
 GROUP BY year
 ORDER BY SUM(sales) DESC
 
@@ -37,25 +41,25 @@ SELECT
 segment,
 COUNT(DISTINCT customer_name) AS Toatl_customers,
 SUM(sales) AS Total_sales
-FROM dbo.SuperStoreOrders
+FROM Sales_data
 GROUP BY segment
 ORDER BY SUM(sales) DESC
 
 -- Shipping and order management --
 SELECT
 ship_mode,
-AVG(shipping_cost) AS Avg_shipping_cost,
-AVG(profit) AS Avg_profit
-FROM dbo.SuperStoreOrders
+Round(AVG(shipping_cost), 2) AS Avg_shipping_cost,
+Round(AVG(profit), 2) AS Avg_profit
+FROM sales_data
 GROUP BY ship_mode
 ORDER BY AVG(profit)
 
 --time analysis --
 SELECT
-ship_mode,
-AVG(DATEDIFF(DAY, TRY_CAST(order_date AS DATE), TRY_CAST(ship_date AS DATE))) AS Avg_time_gape
-FROM dbo.SuperStoreOrders
-GROUP BY ship_mode
+  ship_mode,
+  ROUND(AVG(ship_date - order_date), 2) AS avg_time_gap
+FROM sales_data
+GROUP BY ship_mode;
 
 -- profibility and cost analysis --
 SELECT
@@ -64,7 +68,7 @@ category,
 sub_category,
 AVG(profit) AS Avg_profit,
 AVG(discount) AS Avg_dicount
-FROM dbo.SuperStoreOrders
+FROM sales_data
 GROUP BY 
 product_name,
 category,
@@ -76,7 +80,7 @@ SELECT
 country,
 SUM(sales) AS Total_sales,
 SUM(quantity)  AS Total_quantity
-FROM dbo.SuperStoreOrders
+FROM sales_data
 GROUP BY country
 ORDER BY SUM(sales) DESC
 
@@ -86,7 +90,7 @@ SELECT
 product_name,
 category,
 SUM(quantity) AS Total_quantity_sold
-FROM dbo.SuperStoreOrders
+FROM sales_data
 GROUP BY 
 product_name,
 category
@@ -97,7 +101,7 @@ SELECT
 region,
 sub_category,
 SUM(quantity) AS Total_quantity_sold
-FROM dbo.SuperStoreOrders
+FROM sales_data
 GROUP BY region,
 sub_category
 ORDER BY SUM(quantity) DESC
